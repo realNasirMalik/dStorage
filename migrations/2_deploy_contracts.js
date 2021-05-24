a4842eb6cd236260169845f94b5e01df54d8bd21
realNasirMalik <realNasirMalik@gmail.com>
2021-05-24 15:23:58 -0500

    :art: Connect to UI

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
-//Import Contractconst DStorage = artifacts.require("DStorage");
+const dStorage = artifacts.require("DStorage");
 
 module.exports = function(deployer) {
 	//Deploy Contract
+     deployer.deploy(dStorage);
 };
